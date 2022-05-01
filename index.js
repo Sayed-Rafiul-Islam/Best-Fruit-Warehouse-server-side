4fd9db28966266c8c331b413d5c320b797f5049e
Yasin-Rafid <96468660+Yasin-Rafid@users.noreply.github.com>
2022-05-01 15:11:44 +0600

    .env added and git ignored

--- index.js
+++ index.js
@@ -12,12 +12,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-// user : fruitWarehouse
-// pass : OojbHpJBVZznK0sA
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0bkok.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
