5049ca4b22fd565e2d09907de1c9233f89581b81
Yasin-Rafid <96468660+Yasin-Rafid@users.noreply.github.com>
2022-05-01 17:46:45 +0600

    jwt half done

--- index.js
+++ index.js
@@ -13,6 +13,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+function varifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET
+        , (err, decoded) => {
+            if (err) {
+                return res.status(403).send({ message: 'Forbidden Access' });
+            }
+            console.log('decoded', decoded);
+            req.decoded = decoded;
+            next();
+        })
+    next();
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0bkok.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -37,12 +55,18 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/myItems', async (req, res) => {
+        app.get('/myItems', varifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const query = { email: email };
-            const cursor = itemCollection.find(query);
-            const result = await cursor.toArray();
-            res.send(result);
+            if (decodedEmail === email) {
+                const query = { email: email };
+                const cursor = itemCollection.find(query);
+                const result = await cursor.toArray();
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden Access' })
+            }
         })
 
 
