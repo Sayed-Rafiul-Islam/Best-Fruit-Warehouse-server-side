01ad792c37fdb72ee0b23fad5ca68d237cec2f5a
Sayed Rafiul Islam <sayed80@student.sust.edu>
2023-12-24 12:43:50 +0600

    removed await at client.connect

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        client.connect();
         // database and collections
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
